9908e670b28d680defe97701abd58c95a9e187cd
sbunkerchris <50494698+sbunkerchris@users.noreply.github.com>
2022-07-23 20:33:10 -0600

    fix

--- github-api-nodejs-backend/app.js
+++ github-api-nodejs-backend/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const middlewares = require('./middlewares');
+
 const app = express();
-const PORT=8080;
+const PORT = 8080;
 
 app.use(express.json());
 app.use(cors());
